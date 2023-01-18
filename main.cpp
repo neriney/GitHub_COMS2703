41c7aaa47eb2f6c78cd6d02efdb1f9dc12168562
neriney <ne-ri-ne-y614@outlook.com>
2023-01-19 08:13:26 +0900

    First Commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Sasara Tamaki
+ * @version 0.1
+ * @date 2023-01-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ * Program Description: First Commit to GitHub
+ * 
+ */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
