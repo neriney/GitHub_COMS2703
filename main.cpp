703ee25c1d8198d93fa8e34c330c4bdf7a811fe2
neriney <ne-ri-ne-y614@outlook.com>
2023-01-19 08:21:21 +0900

    second commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,4 @@
-/**
- * @file main.cpp
- * @author Sasara Tamaki
- * @version 0.1
- * @date 2023-01-19
- * 
- * @copyright Copyright (c) 2023
- * 
- * Program Description: First Commit to GitHub
- * 
- */
+// Sasara Tamaki
 
 #include<iostream>
 using namespace std;
